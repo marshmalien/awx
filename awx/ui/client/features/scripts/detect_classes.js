acb171153df499c1f1116ff59c92267168cfc406
kialam <digitalanime@gmail.com>
2018-08-28 11:19:26 -0400

    remove unused js file

--- awx/ui/client/features/scripts/detect_classes.js
+++
@@ -1,8 +0,0 @@
-const unique = arr => {
-    let prev;
-    return arr.sort().filter(e => e !== prev && (prev = e));
-}
-
-let listOfClasses = [].concat(...[...document.querySelectorAll('[class]')].map(el => [...el.classList]));
-
-console.log(unique(listOfClasses));
